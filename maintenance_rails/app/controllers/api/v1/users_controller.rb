c7bdc83d2d49206c4beb26b436b769cbc4975622
melonybox <grandmadcow@gmail.com>
2020-03-31 01:33:44 -0400

    user items created, user controller auth

---
+++ maintenance_rails/app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+  user = User.new(
+    email: params[:email],
+    username: params[:username],
+    password: params[:password],
+  )
+  if user.save
+    token = encode_token(user.id)
+    render json: {user: UserSerializer.new(user), token: token}
+    # token would be used if autologin was implemented or possibly else?
+  else
+    render json: {errors: user.errors.full_messages}
+  end
+end
+end
