67fd4951a4d9a24950f7a09803224d47b95da300
melonybox <grandmadcow@gmail.com>
2020-03-31 01:18:48 -0400

    app controller with auth

--- maintenance_rails/app/controllers/application_controller.rb
+++ maintenance_rails/app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+  def encode_token(user_id)
+    JWT.encode({user_id: user_id}, "bananaTwinkies")
+  end
+  # encode user to jwt tokent
+
+  def get_auth_headers
+    request.headers["Authorization"]
+  end
+  # only work if headers has this string
+
+  def decode_token
+    begin
+      JWT.decode(get_auth_headers, "bananaTwinkies")[0]["user_id"]
+    rescue
+      nil
+    end
+  end
+  # decode user token for autologin, not used
+
+  def session_user
+    User.find_by(id: decode_token)
+  end
 end
