6943b2f0725179417eb5da999eccf10ceecf5836
melonybox <grandmadcow@gmail.com>
2020-04-08 12:15:52 -0400

    Reducer for login

--- maintenance_react/src/reducers/reducer.js
+++ maintenance_react/src/reducers/reducer.js
@@ -1,1 +1,12 @@
+const initialState = {
+  currentUser: {},
+}
 
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+      case 'LOGIN_USER':
+        return {...state, currentUser: action.payload, bodyView: 'portfolio'}
+      default:
+        return state;
+  }
+}
