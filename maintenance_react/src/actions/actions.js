3f15691e1f3c9e3cee2989a565756e9d5e87c054
melonybox <grandmadcow@gmail.com>
2020-04-08 12:18:48 -0400

    Login user action

--- maintenance_react/src/actions/actions.js
+++ maintenance_react/src/actions/actions.js
@@ -1,3 +1,30 @@
+export const userLoginFetch = data => {
+  //data contains email and password
+  return dispatch => {
+    //this is part of redux-thunk
+    return fetch("https://localhost:3000/api/v1/login", {
+      //fetch to backend rails api
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(data)
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        if (data.errors) {
+          // Here you should have logic to handle invalid login credentials.
+          // This assumes your Rails API will return a JSON object with a key of
+          // 'message' if there is an error
+          alert(data.errors)
+        } else {
+          dispatch(loginUser(data.user))
+        }
+      })
+  }
+}
+
 export const loginUser = data => ({
   type: 'LOGIN_USER',
   payload: data
