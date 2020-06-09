7bc2d8227e96624030ee56bdecaaf02d6b0c911d
melonybox <grandmadcow@gmail.com>
2020-06-08 23:22:23 -0400

    ORM comments

--- maintenance_rails/db/migrate/20200331053230_create_users.rb
+++ maintenance_rails/db/migrate/20200331053230_create_users.rb
@@ -9,3 +9,13 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     end
   end
 end
+
+# user has many overviews
+# overviews has many items
+# item has many item sub section
+# item sub section has many item sub section part
+
+# overview belongs to user
+# item belongs to overview
+# item sub section belongs to item
+# item sub section part belongs to item sub
