8e87f6a31254807e84ac910281bb61fba9626bc9
melonybox <grandmadcow@gmail.com>
2020-07-11 15:23:48 -0400

    user model added, uuid test

--- maintenance_rails/app/controllers/api/v1/item_subsections_controller.rb
+++
@@ -1,2 +0,0 @@
-class Api::V1::ItemSubsectionsController < ApplicationController
-end
--- maintenance_rails/config/routes.rb
+++ maintenance_rails/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-    
+      resources :users
+
       get "/auto_login", to: "auth#auto_login"
       post "/login", to: "auth#login"
     end
