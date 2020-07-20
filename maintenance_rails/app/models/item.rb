02091694c060583105bbb13f388df32f5cce85c3
melonybox <grandmadcow@gmail.com>
2020-07-20 17:56:46 -0400

    Item orm
    d
    qq
    quit
    exit

--- maintenance_rails/app/models/item.rb
+++ maintenance_rails/app/models/item.rb
@@ -1,2 +1,6 @@
 class Item < ApplicationRecord
+  self.implicit_order_column = "created_at"
+
+  has_many :sub_items, class_name: "Item", foreign_key: "main_item_id"
+  belongs_to :main_item, class_name: "Item", optional: true
 end
--- maintenance_rails/db/migrate/20200720215245_create_items.rb
+++ maintenance_rails/db/migrate/20200720215245_create_items.rb
@@ -1,6 +1,8 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items, id: :uuid do |t|
+      t.string :name
+      t.uuid :main_item_id
 
       t.timestamps
     end
