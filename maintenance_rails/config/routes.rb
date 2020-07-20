62ecd90fb08cd674b35b76b86694799fc0623c44
melonybox <grandmadcow@gmail.com>
2020-07-20 17:53:58 -0400

    item resource created.

--- maintenance_rails/config/routes.rb
+++ maintenance_rails/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users
       resources :projects
+      resources :items
 
       get "/auto_login", to: "auth#auto_login"
       post "/login", to: "auth#login"
