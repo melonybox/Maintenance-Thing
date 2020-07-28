d6925eac97bd187838ac03afd82d7a0b75ff823a
melonybox <grandmadcow@gmail.com>
2020-07-28 13:39:22 -0400

    root project list view command experiment success

--- maintenance_rails/app/models/project.rb
+++ maintenance_rails/app/models/project.rb
@@ -9,3 +9,4 @@ end
 # -> f79b440b-7821-488a-80cb-00f62b3ddb42
 # -> 37eea4a1-b1c0-4793-9d59-496f90a68738
 # f9bca3b1-89c0-4fbf-8756-c1478e81ff6e sub_projects.empty? = true, this has no sub projects
+# Project.where(main_project_id: nil), to be used as getting root projects displayed
