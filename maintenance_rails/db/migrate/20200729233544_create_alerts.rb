81b44410e81f408aeca45ecd3af7c90de387fd35
melonybox <grandmadcow@gmail.com>
2020-07-29 20:04:15 -0400

    alert table formating, looking into how to properly store money value

--- maintenance_rails/db/migrate/20200729233544_create_alerts.rb
+++ maintenance_rails/db/migrate/20200729233544_create_alerts.rb
@@ -1,6 +1,11 @@
 class CreateAlerts < ActiveRecord::Migration[6.0]
   def change
     create_table :alerts, id: :uuid do |t|
+      t.uuid :item_id
+      t.string :description
+      t.string :interval
+      t.decimal :cost
+
 
       t.timestamps
     end
