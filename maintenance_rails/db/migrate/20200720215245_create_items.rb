bfc54d668dd207afde0ad55d51731645335e8150
melonybox <grandmadcow@gmail.com>
2020-08-04 19:56:30 -0400

    projects resource to be removed, foreign key set to uuid via generators to solve issues.

--- maintenance_rails/db/migrate/20200720215245_create_items.rb
+++ maintenance_rails/db/migrate/20200720215245_create_items.rb
@@ -3,6 +3,8 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items, id: :uuid do |t|
       t.string :name
       t.uuid :main_item_id
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :alert, foreign_key: true
 
       t.timestamps
     end
--- maintenance_rails/db/schema.rb
+++ maintenance_rails/db/schema.rb
@@ -10,32 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_215245) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pgcrypto"
   enable_extension "plpgsql"
-
-  create_table "items", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "name"
-    t.uuid "main_item_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "projects", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "name"
-    t.uuid "main_project_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
