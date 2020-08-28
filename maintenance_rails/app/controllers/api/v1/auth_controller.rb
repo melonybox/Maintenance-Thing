fe4e1b91ca604c697bb651c4f9819fb53828c5bc
melonybox <grandmadcow@gmail.com>
2020-08-28 04:16:37 -0400

    more google auth stuff

--- maintenance_rails/app/controllers/api/v1/auth_controller.rb
+++ maintenance_rails/app/controllers/api/v1/auth_controller.rb
@@ -17,4 +17,19 @@ class Api::V1::AuthController < ApplicationController
       render json: {errors: "Cookies have been tampered with."}
     end
   end
+
+  def googleAuth
+    #get access tokens from google server
+    access_token = request.env["omniauth.auth"]
+    user = User.from_omniauth(access_token)
+    log_in(user)
+    #access_token is used to authenticate request made from the rails app to the google server
+    user.google_token = access_token.credentials.token
+    #refresh_token to request new access_token
+    #note: refresh_token is only sent once during the first request
+    refresh_token = access_token.credentials.refresh_token
+    user.google_refresh_token = refresh_token if refresh_token.present?
+    user.save
+    redirect_to root_path
+  end
 end
--- maintenance_rails/app/controllers/api/v1/users_controller.rb
+++ maintenance_rails/app/controllers/api/v1/users_controller.rb
@@ -12,4 +12,12 @@ class Api::V1::UsersController < ApplicationController
   else
     render json: {errors: user.errors.full_messages}
   end
+
+  def self.from_omniauth(auth)
+    #creates new user only if it doesn't exist
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+    end
+  end
 end
