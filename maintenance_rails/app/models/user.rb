8f3d6179c1aaab14802182a0dbcd0488691852ed
melonybox <grandmadcow@gmail.com>
2020-08-28 04:26:12 -0400

    further google auth edits

--- maintenance_rails/app/models/user.rb
+++ maintenance_rails/app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-  has_secure_password
   self.implicit_order_column = "created_at"
 
   validates :email, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
-  validates :username, presence: true
-  validates :password, presence: true
+  validates :name, presence: true
 
   has_many :items
   has_many :alerts, through: :items
--- maintenance_rails/db/migrate/20200711193011_create_users.rb
+++ maintenance_rails/db/migrate/20200711193011_create_users.rb
@@ -1,6 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users, id: :uuid do |t|
+      t.string :name
+      t.string :email
       t.string :google_token
       t.string :google_refresh_token
 
