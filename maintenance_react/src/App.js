66514ba9adbe0bcda6c9794fd03a9ac550e062e4
melonybox <grandmadcow@gmail.com>
2020-09-03 15:50:24 -0400

    Adding google sign in partly works, needs editing to be complete.

--- maintenance_react/src/App.js
+++ maintenance_react/src/App.js
@@ -3,10 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div class="g-signin2" dataOnsuccess={onSignIn}></div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
