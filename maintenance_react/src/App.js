c6d8c281c66aa4d034ed8de42d636ce5046f3b95
melonybox <grandmadcow@gmail.com>
2020-09-03 17:11:01 -0400

    Getting tokens / data from google login, next is getting it work on backend

--- maintenance_react/src/App.js
+++ maintenance_react/src/App.js
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  function onSignIn(googleUser) {
+class App extends Component {
+
+  onSignIn = (googleUser) => {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    debugger
   }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div class="g-signin2" dataOnsuccess={onSignIn}></div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <GoogleLogin
+            clientId="1020775263438-kiovhnjri4lad9gvr281hk0rlv6mab9m.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={this.onSignIn}
+            onFailure={this.onSignIn}
+            cookiePolicy={'single_host_origin'}
+          />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
